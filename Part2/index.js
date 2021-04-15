7aa3877672f6357a05ef510ce102cca76699a0cc
Hannah Shin <hannahshin@Hannahs-MBP.lan>
2021-04-14 22:17:55 -0700

    part 2 hw

---
+++ Part2/index.js
@@ -0,0 +1,12 @@
+const idInput = document.getElementById('idInput');
+const colorInput = document.getElementById('colorInput');
+
+function setCard(){
+    const card = document.getElementById(idInput.value);
+    console.log(card);
+    card.style.color = colorInput.value; 
+}
+
+function reset(){
+    
+}
